3f1c8a9951eb734829ce1cd34de25ccc10c1ed14
kaiquepill <kaique.gomes@bandtec.com.br>
2021-01-15 19:03:43 -0300

    teste

---
+++ merchant-service/src/main/kotlin/com/foodltda/merchantservice/MerchantServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.foodltda.merchantservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MerchantServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MerchantServiceApplication>(*args)
+}
