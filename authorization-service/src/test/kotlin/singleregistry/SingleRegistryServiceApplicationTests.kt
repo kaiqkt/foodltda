a453f13380c927ca09168bcf22cbb401dd0c63bf
Kaique <kaique.gomes@bandtec.com.br>
2021-02-19 15:09:18 -0300

    setup project

---
+++ authorization-service/src/test/kotlin/singleregistry/SingleRegistryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package singleregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SingleRegistryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
