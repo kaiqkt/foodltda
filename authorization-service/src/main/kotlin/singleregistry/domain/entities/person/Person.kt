8e83c0ba1bc34f4540399c3f66ed4935fe68610e
Kaique <kaique.gomes@bandtec.com.br>
2021-02-21 21:31:18 -0300

    feat: person document annotation

--- authorization-service/src/main/kotlin/singleregistry/domain/entities/person/Person.kt
+++ authorization-service/src/main/kotlin/singleregistry/domain/entities/person/Person.kt
@@ -1,11 +1,13 @@
 package singleregistry.domain.entities.person
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import singleregistry.domain.entities.Address
 import singleregistry.domain.entities.PersonType
 import singleregistry.domain.entities.Phone
 import java.time.LocalDateTime
 
+@Document
 data class Person(
     @Id
     val personId: String? = null,
