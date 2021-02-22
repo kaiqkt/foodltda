5bc8f10ececf1a1055a204e1e913f83431f3f620
Kaique <kaique.gomes@bandtec.com.br>
2021-02-21 21:30:28 -0300

    feat: error handler created with DataValidationException.kt and DomainException.kt

---
+++ authorization-service/src/main/kotlin/singleregistry/domain/exceptions/DomainException.kt
@@ -0,0 +1,8 @@
+package singleregistry.domain.exceptions
+
+abstract class DomainException : Exception {
+
+    constructor() : super()
+    constructor(message: String?) : super(message)
+    open fun details(): List<String> = arrayListOf()
+}
