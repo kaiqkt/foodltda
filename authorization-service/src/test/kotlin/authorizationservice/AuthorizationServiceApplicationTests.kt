802d0c17a194d047cb7bd7865ebf5d9d2a3d4bc4
Kaique <kaique.gomes@bandtec.com.br>
2021-03-18 20:15:31 -0300

    feat: setup project

---
+++ authorization-service/src/test/kotlin/authorizationservice/AuthorizationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package authorizationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
